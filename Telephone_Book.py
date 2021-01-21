b84c5dd87076c11af4acb0fe3051fbbf8027d409
Akari Johnson <akarijohnson.hgs@Akaris-MacBook-Pro.local>
2021-01-20 18:15:05 -0800

    intiatial commit

---
+++ Telephone_Book.py
@@ -0,0 +1,10 @@
+import pickle
+
+# 1. Leader Numbers
+leader{}
+leader["Jacore Baptiste"] = '(845) 200-6250'
+leader["Josiah Johnson"] = '(510) 860-5112'
+leader["Richard Kamau"] = '(510) 228-5623'
+leader["Andrew Lubega"] = '(925) 727-4611'
+leader["Aris Cater"] = '(845) 200-6250'
+leader["Gaberiel"] = '(845) 200-6250'
